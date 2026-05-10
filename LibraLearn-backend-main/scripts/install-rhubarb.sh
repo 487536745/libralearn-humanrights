a604b1381d08efc91d3cb9fc7a5ed65cc73cadac
Muhammad ahad farooq <ahadfarooq76@gmail.com>
2026-05-10 01:21:14 -0700

    Fix Render Rhubarb install to use repo-local binary and remove stale RHUBARB_PATH

--- LibraLearn-backend-main/scripts/install-rhubarb.sh
+++ LibraLearn-backend-main/scripts/install-rhubarb.sh
@@ -11,16 +11,12 @@ RHUBARB_VERSION="1.14.0"
 ZIP_NAME="Rhubarb-Lip-Sync-${RHUBARB_VERSION}-Linux.zip"
 DOWNLOAD_URL="https://github.com/DanielSWolf/rhubarb-lip-sync/releases/download/v${RHUBARB_VERSION}/${ZIP_NAME}"
 
-mkdir -p bin
-
 tmp_dir="$(mktemp -d)"
 
 wget -O "${tmp_dir}/rhubarb.zip" "${DOWNLOAD_URL}"
-
 unzip -q "${tmp_dir}/rhubarb.zip" -d "${tmp_dir}"
-
+mkdir -p bin
 cp "${tmp_dir}/Rhubarb-Lip-Sync-${RHUBARB_VERSION}-Linux/rhubarb" ./bin/rhubarb
-
 chmod +x ./bin/rhubarb
 
 echo "Rhubarb installed at ./bin/rhubarb"
